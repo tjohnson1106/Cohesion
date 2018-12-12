981cc3234b1fe8cfe814f64735f6ac1c444e0ce9
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-12 17:08:29 -0500

    created query renderer initial setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,54 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
+import React from "react";
+import {
+  AppRegistry,
+  View,
+  Text,
+  Platform,
+  Dimensions,
+  StyleSheet
+} from "react-native";
+import { QueryRenderer, graphql } from "react-relay";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
+import ErrorBoundary from "./components/ErrorBoundary";
+import Page from "./pages";
+import Screen from "./screens";
+import environment from "./environment";
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+const App = () => (
+  <ErrorBoundary>
+    <Router>
+      <QueryRenderer
+        environment={environment}
+        render={({ error, props }) => {
+          //console.log(props);
+          if (error) {
+            return (
+              <View>
+                <Text>{error.message}</Text>
+              </View>
+            );
+          } else if (props && Platform.OS === "web") {
+            return (
+              <ErrorBoundary>
+                <Screen data={props} />
+              </ErrorBoundary>
+            );
+          } else if (props) {
+            return (
+              <ErrorBoundary>
+                <Screen data={props} />
+              </ErrorBoundary>
+            );
+          } else {
+            return (
+              <View>
+                <Text>Loading...</Text>
+              </View>
+            );
+          }
+        }}
+      />
+    </Router>
+  </ErrorBoundary>
+);
