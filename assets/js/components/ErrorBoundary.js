7886430531730a13d1961aba78c93de24687487d
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-12 17:18:06 -0500

    created error boundary component

---
+++ assets/js/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppRegistry, View, Text } from "react-native";
+
+export default class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error) {
+    this.setState({ error });
+  }
+
+  render() {
+    const { error } = this.state;
+
+    return error ? (
+      <View>
+        <Text>{error.toString()}</Text>
+      </View>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+AppRegistry.registerComponent("ErrorBoundary", () => ErrorBoundary);
