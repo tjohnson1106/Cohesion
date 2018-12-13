64f343754b96626d6977977d79f83cf536fa5170
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-13 16:34:28 -0500

    created initial relay environment

---
+++ assets/js/environment.js
@@ -0,0 +1,12 @@
+import {
+  createFetcher,
+  createSubscriber
+} from "@absinthe/socket-relay/compat/cjs";
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+
+import absintheSocket from "./absinthe";
+
+export default new Environment({
+  network: Network.create(createFetcher(absintheSocket)),
+  store: new Store(new RecordSource())
+});
